aff037f0779b81b4f0a4d5838bf081da5964a1ac
Vacarme27 <kevin.trillon@gmail.com>
2023-09-21 14:33:34 +0200

    add components + view + routing

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,20 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import HomePage from './View/Homepage'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import HomePage from './view/homepage/Homepage';
+import Footer from './components/footer/footer';
+import Header from './components/header/header';
+import SignIN from './view/sign-in/sign-in';
+import './app.scss';
+
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/sign-in' element={<SignIN/>}/>        
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
